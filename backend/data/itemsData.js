a5ae53add39aa05112bcd7387accb1c980e99fbe
amoizzahid18 <a.moiz.zahid.18@gmail.com>
2024-11-18 10:33:48 +0500

    modify itemsData

--- backend/data/itemsData.js
+++ backend/data/itemsData.js
@@ -1,3 +1,13 @@
 const itemsData = [
-    {}
-]
+    { "name": "Qorma / Curry" },
+    { "name": "Pulao / Biryani" },
+    { "name": "Chinese Cuisine" },
+    { "name": "Vegetables" },
+    { "name": "Bbq" },
+    { "name": "Halwa Puri Combo" },
+    { "name": "Beverages" },
+    { "name": "Tandoor" },
+    { "name": "Dessert" },
+    { "name": "Add Ons" }
+  ]
+  
