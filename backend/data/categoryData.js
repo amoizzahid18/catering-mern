fa5025aeab3d91162f9c0abe361de4ea00313e1e
amoizzahid18 <a.moiz.zahid.18@gmail.com>
2024-11-18 10:35:03 +0500

    modify categoryData

---
+++ backend/data/categoryData.js
@@ -0,0 +1,12 @@
+const categoryData = [
+    { "name": "Qorma / Curry" },
+    { "name": "Pulao / Biryani" },
+    { "name": "Chinese Cuisine" },
+    { "name": "Vegetables" },
+    { "name": "Bbq" },
+    { "name": "Halwa Puri Combo" },
+    { "name": "Beverages" },
+    { "name": "Tandoor" },
+    { "name": "Dessert" },
+    { "name": "Add Ons" }
+  ]
--- backend/data/itemsData.js
+++ backend/data/itemsData.js
@@ -1,13 +1,2 @@
-const itemsData = [
-    { "name": "Qorma / Curry" },
-    { "name": "Pulao / Biryani" },
-    { "name": "Chinese Cuisine" },
-    { "name": "Vegetables" },
-    { "name": "Bbq" },
-    { "name": "Halwa Puri Combo" },
-    { "name": "Beverages" },
-    { "name": "Tandoor" },
-    { "name": "Dessert" },
-    { "name": "Add Ons" }
-  ]
+
   
