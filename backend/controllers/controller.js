2ab7e77c862944e4174484e407379913d4e85af3
amoizzahid18 <a.moiz.zahid.18@gmail.com>
2024-11-18 19:58:52 +0500

    get router for items

--- backend/controllers/controller.js
+++ backend/controllers/controller.js
@@ -13,6 +13,18 @@ const getAllCategories = async (req, res) => {
     }
 }
 
+
+const getAllItems = async(req, res) => {
+    try {
+        const data = await Item.find({});
+        res.status(200).json(data);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({message: error.message})  
+    }
+}
+
 module.exports = {
     getAllCategories,
+    getAllItems,
 }
--- backend/routes/route.js
+++ backend/routes/route.js
@@ -1,10 +1,15 @@
 const express = require("express");
 const router = express.Router();
 
-const {getAllCategories} = require("../controllers/controller.js");
+const {getAllCategories, 
+    getAllItems,
+    } = require("../controllers/controller.js");
 
 
 // get all caegories 
 router.get("/categories", getAllCategories)
+
+//get all Items
+router.get("/items", getAllItems)
 
 module.exports = router;
